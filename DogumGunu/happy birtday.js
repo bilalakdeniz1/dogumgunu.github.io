10f41aa05286b0bcfd0e36f6624dc0fc4f4744ee
bilalakdeniz1 <dogumgunuwibesite@gmail.com>
2024-12-04 16:55:50 +0300

    Update happy birtday.js

--- DogumGunu/happy birtday.js
+++ DogumGunu/happy birtday.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["İYİKİ", "DOĞDUN", "RAVZA"],
+    strings: ["İYİKİ", "DOĞDUN", "YİĞİT"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
